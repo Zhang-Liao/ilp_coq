d013330836ec822613ba46552ccd1a63a19bfeb0
Zhang-Liao <zhangliao714@gmail.com>
2023-06-23 17:17:35 +0200

    preprocess

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 sys.path.append(os.path.dirname(sys.path[0]))
 from lib.global_setting import *
 
---
+++ preprocess/preproc.py
@@ -0,0 +1,28 @@
+# import shutil
+import os
+import sys
+sys.path.append(os.path.dirname(sys.path[0]))
+from lib import global_setting
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str)
+opts = parser.parse_args()
+
+input = opts.file
+base  = os.path.splitext(input)[0]
+f_feat = os.path.splitext(input)[0] + ".feat"
+f_label = os.path.splitext(input)[0] + ".label"
+
+def mk_feat(f):
+    return (" ".join(["%s:%s" % (feat, num) for (feat, num) in f]))
+
+with open(input, 'r') as reader, open(f_feat, 'a') as w_feat, open(f_label, 'a') as w_label:
+    for l in reader:
+        l = l.strip()
+        if global_setting.lemma_delimiter not in l:
+            l = json.loads(l)
+            feat = mk_feat(l['feats'])
+            w_feat.write(feat + '\n')
+            w_label.write(l['tac'] + '\n')
