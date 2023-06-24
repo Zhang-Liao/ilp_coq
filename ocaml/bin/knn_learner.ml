b6621c48f8bbdd52686c44abf9e877a928d4f32e
Zhang-Liao <zhangliao714@gmail.com>
2023-06-24 21:11:48 +0200

    no circle queue

--- ocaml/bin/knn_learner.ml
+++ ocaml/bin/knn_learner.ml
@@ -24,42 +24,6 @@ let knn f1 f2 =
 (* let u = float_of_int @@ List.length@@union f1 f2 in
    i /. u *)
 
-module CircularQueue : sig
-  type 'a t
-
-  val empty : int -> 'a t
-  val add : 'a -> 'a t -> 'a option * 'a t
-  val to_list_map : ('a -> 'b) -> 'a t -> 'b list
-  val size : 'a t -> int
-  val max : 'a t -> int
-end = struct
-  type 'a t = { max : int; size : int; incoming : 'a list; outgoing : 'a list }
-
-  let empty max = { size = 0; max; incoming = []; outgoing = [] }
-  let head_tail ls = (List.hd ls, List.tl ls)
-
-  let add x { max; size; incoming; outgoing } =
-    if size < max then (
-      assert (outgoing = []);
-      (None, { max; size = size + 1; incoming = x :: incoming; outgoing }))
-    else (
-      assert (size = max);
-      match outgoing with
-      | [] ->
-          let out, outgoing = head_tail @@ List.rev (x :: incoming) in
-          (Some out, { max; size; incoming = []; outgoing })
-      | out :: outgoing ->
-          (Some out, { max; size; incoming = x :: incoming; outgoing }))
-
-  let to_list_map f { incoming; outgoing; _ } =
-    let tail = List.fold_right (fun x -> List.cons (f x)) outgoing [] in
-    List.fold_left (fun ls x -> List.cons (f x) ls) tail incoming
-
-  let size { size; _ } = size
-  let max { max; _ } = max
-end
-
-
 module Tactic = struct
   type t = string
 
@@ -73,16 +37,14 @@ type db_entry = {
   features : int list;
 }
 
-let add db (features, tac) =
-  let in_db = { obj = tac; features } in
-  snd @@ CircularQueue.add in_db db
+let add db (features, obj) = { obj; features } :: db
 
 let knn_dist ps db =
   let dist x = (knn ps x.features, x.obj) in
   let pds =
     List.sort
       (fun (d1, _) (d2, _) -> Float.compare d1 d2)
-      (CircularQueue.to_list_map dist db)
+      (List.rev_map dist db)
   in
   List.map snd pds
 
