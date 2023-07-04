02fcffd937d98c657dd442a82cba4e1eb6ad6242
Zhang-Liao <zhangliao714@gmail..com>
2023-07-04 20:19:45 +0000

    to predc3

--- preproc/json_to_predc.py
+++ preproc/json_to_predc.py
@@ -7,9 +7,8 @@ from lib import global_setting
 pos_neg_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split/1000_neg.json'
 json_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/1000.json'
 bk_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/simpl_bk.pl'
-pos_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/simpl_pos.pl'
-neg_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/simpl_neg.pl'
-
+pos_neg_prolog_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/simpl_pn.pl'
+tac = 'simpl'
 def idx_str(idx):
     idx = ",".join(idx)
     idx = "[" + idx +"]"
@@ -23,7 +22,7 @@ def goal_predc(i, l, writer):
     for ident, idx in l:
         writer.write("{}({},{}).\n".format(ident, i, idx_str(idx)))
 
-def output(kind, pos_neg, out):
+def pr_bk(pos_neg, out):
     with (
         open(json_file, 'r') as reader,
         open(out, 'a') as writer):
@@ -31,27 +30,35 @@ def output(kind, pos_neg, out):
         for l in reader:
             l = l.strip()
             if global_setting.lemma_delimiter not in l:
-                l = json.loads(l)
-                if (kind == 'pos') & (i in pos_neg['pos']):
-                    hyps_predc(i, l['hyps'], writer)
-                    goal_predc(i, l['goal'], writer)
-                elif (kind == 'neg') & (i in pos_neg['neg']):
+                if i in pos_neg:
+                    l = json.loads(l)
                     hyps_predc(i, l['hyps'], writer)
                     goal_predc(i, l['goal'], writer)
                 i += 1            
-    
-with open(pos_neg_file, 'r') as r:
+
+def pr_predc(pos, neg, out):
+    with open(out, 'a') as writer:
+        writer.write(":-begin_in_pos.\n")    
+        for p in pos:
+            writer.write("{}({})\n".format(tac,p))
+        writer.write(":-end_in_pos.\n")                
+        writer.write(":-begin_in_neg.\n")   
+        for n in neg:
+            writer.write("{}({})\n".format(tac,n))
+        writer.write(":-end_in_neg.\n")   
+
+
+
+with open(json_file, 'r') as r:
     pos_neg_dict = json.load(r) 
-    pos_neg = pos_neg_dict['simpl']
+    tac_pos_neg = pos_neg_dict[tac]
+    pos_neg = tac_pos_neg['pos'] + tac_pos_neg['neg']
 
 if os.path.exists(bk_file):
     os.remove(bk_file)
 
-if os.path.exists(pos_file):
-    os.remove(pos_file)
-
-if os.path.exists(neg_file):
-    os.remove(neg_file)
-    
-output('pos', pos_neg, pos_file)
-output('neg', pos_neg, neg_file)
+if os.path.exists(pos_neg_prolog_file):
+    os.remove(pos_neg_prolog_file)
+        
+pr_bk(pos_neg, bk_file)
+pr_predc(tac_pos_neg['pos'], tac_pos_neg['neg'], pos_neg_prolog_file)
