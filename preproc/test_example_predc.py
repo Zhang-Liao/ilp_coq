ed5d246dc357471be284fc23e7823cb94cf52ff8
Zhang-Liao <zhangliao714@gmail.com>
2023-08-05 22:43:11 +0800

    test exg predc

---
+++ preproc/test_example_predc.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+sys.path.append(os.path.dirname(sys.path[0]))
+
+from lib import global_setting
+from lib import utils
+
+test = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8.json'
+out_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8/test_predc'
+
+def init_out(i):
+    out = os.path.join(out_dir, f'{i}.pl')
+    if os.path.exists(out):
+        os.remove(out)
+    return out
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+with open(test, 'r') as r:
+    i = 0
+    for l in r:
+        l = l.strip()
+        if global_setting.lemma_delimiter not in l:
+            l = json.loads(l)
+            out = init_out(i)
+            with open(out, 'a') as w:
+                w.write(':-style_check(-discontiguous).\n')
+                utils.pr_goal_predc(i, l['goal'], w)
+                utils.pr_hyps_predc(i, l['hyps'], w)
+        i += 1
