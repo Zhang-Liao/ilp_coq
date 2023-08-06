5e69601da441e11e7899740a65bf705c2ab30308
Zhang-Liao <zhangliao714@gmail.com>
2023-08-06 18:30:03 +0800

    rand split by file order

--- preproc/random_split.py
+++ preproc/random_split.py
@@ -1,16 +1,16 @@
 import os
 import sys
-from pathlib import Path
 sys.path.append(os.path.dirname(sys.path[0]))
 from lib import global_setting
 from lib import utils
 
 lemma_split_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/lemma_split'
-dataset = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/syn_vertical2'
+dataset = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/origin_feat'
+file_order = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/file_order'
 out_dir = os.path.join(dataset, 'ten_split')
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
-    
+
 def load_split_lemmas():
     split_lemmmas = []
     for root, _, files in os.walk(lemma_split_dir):
@@ -39,22 +39,27 @@ def output_lemma(data, lemma, split_lemmas):
         print(lemma+"not in any split")
 
 split_lemmas = load_split_lemmas()
-for root, _, files in os.walk(dataset):
-    for file in files:
+with open(file_order, 'r') as r:
+    files = r.readlines()
+    files = [f.strip() for f in files]
+
+for f in files:
+    path = os.path.join(dataset, f)
+    if not os.path.exists(path):
+        raise FileNotFoundError(f)
+
+    with open(path, 'r') as reader:
         lemma_states = []
-        if Path(file).suffix == '.json':
-            path = os.path.join(root, file)
-            with open(path, 'r') as reader:
-                for line in reader:
-                    line = line.strip()
-                    if global_setting.lemma_delimiter not in line:
-                        lemma_states.append(line)
-                    else:
-                        if lemma_states != []:
-                            # print('append lemma with', len(lemma_states), 'states')
-                            output_lemma(lemma_states, lemma, split_lemmas)
-                        lemma = utils.lemma_name(line)
-                        lemma_states = []
-                # the last lemma in a file
+        for line in reader:
+            line = line.strip()
+            if global_setting.lemma_delimiter not in line:
+                lemma_states.append(line)
+            else:
                 if lemma_states != []:
+                    # print('append lemma with', len(lemma_states), 'states')
                     output_lemma(lemma_states, lemma, split_lemmas)
+                lemma = utils.lemma_name(line)
+                lemma_states = []
+        # the last lemma in a file
+        if lemma_states != []:
+            output_lemma(lemma_states, lemma, split_lemmas)
