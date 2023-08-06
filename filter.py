6932974a757be6a7c4c70309c84c0bc1e9aca282
Zhang-Liao <zhangliao714@gmail.com>
2023-08-06 17:49:12 +0800

    filter continue error

--- filter.py
+++ filter.py
@@ -1,4 +1,3 @@
-import copy
 import os
 import sys
 sys.path.append(os.path.dirname(sys.path[0]))
@@ -6,10 +5,12 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from pyswip import Prolog
 
 from lib import global_setting
+from lib import utils
 
-clause_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/'
-pred_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/test.pred'
-example_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/'
+clause_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/1000/predc_auto'
+pred_file = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/ten_split/06-27-2023-10:31:58/split8.eval'
+example_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8/test_predc'
+all_predc = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/all_predc.pl'
 
 def read_cls_paths():
     cls_paths = {}
@@ -36,14 +37,28 @@ def read_exg_paths():
 
 def filter_row(i, r, exg_paths, cls_paths):
     new_preds = []
-    preds = r.split()
+    preds = r.split('\t')
     for p in preds:
-        prolog = Prolog()
-        prolog.consult(exg_paths[i])
-        prolog.consult(cls_paths[p])
-        # query fail if return []
-        if list(prolog.query(f'tac({i})')) != []:
-            new_preds.append(p)
+        p = utils.tac_as_file(p)
+        # print('pred', p)
+        # print(cls_paths.keys())
+        # exit()
+        if p in cls_paths.keys():
+            # print('has clause for tac')
+            prolog = Prolog()
+            # prolog.consult(all_predc)
+            prolog.consult(exg_paths[i])
+            prolog.consult(cls_paths[p])
+            # query fail if return []
+            try:
+                if list(prolog.query(f'tac({i})')) != []:
+                    new_preds.append(p)
+            except:
+                # TODO: better solution instead of ignoring the error.
+                # Clause may contain predicates that not in the example. Now, I
+                # treat it as failure and continue. Add all predicates initially
+                # seems cause warnings in Prolog.
+                ()
     print(new_preds)
     return new_preds
 
@@ -58,6 +73,6 @@ def filter(exg_paths, cls_paths):
             i += 1
 
 exg_paths = read_exg_paths()
-print('exg_paths', exg_paths)
+# print('exg_paths', exg_paths)
 cls_paths = read_cls_paths()
 filter(exg_paths, cls_paths)
