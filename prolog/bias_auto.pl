2b3da1bb09a487f080e67da2f66e4b1d81f0a981
Zhang-Liao <zhangliao714@gmail.com>
2023-08-11 21:52:10 +0800

    left not only siblings

--- prolog/bias_auto.pl
+++ prolog/bias_auto.pl
@@ -1,27 +1,37 @@
 name_equal(X, Y) :- X = Y.
-position_left([X], [Y]) :- !, integer(X), integer(Y), X < Y.
-position_left([H|X], [H|Y]) :- position_left(X, Y).
-position_above(X, Y) :- dif(X,Y), prefix(X, Y).
-
-nat_idx([]).
-nat_idx([H | Tl]) :- integer(H), nat_idx(Tl).
-goal_idx(Idx) :- nat_idx(Idx).
 
 hyp_typ(hyp_ass).
 hyp_typ(hyp_dc).
 hyp_typ(hyp_dt).
+
+position_left([X], [Y]) :- !, integer(X), integer(Y), X < Y.
+position_left([H|X], [H|Y]) :- position_left(X, Y).
+
+hyp_position_left([Name, Type| Idx1], [Name, Type| Idx2]) :-
+    hyp_typ(Type), position_left(Idx1, Idx2).
+
+goal_position_left(Idx1, Idx2) :- position_left(Idx1, Idx2).
+
+position_above(X, Y) :- dif(X,Y), prefix(X, Y).
+
+nat_idx([]).
+nat_idx([H | Tl]) :- integer(H), nat_idx(Tl).
+
+goal_idx(Idx) :- nat_idx(Idx).
+
 hyp_idx([_, Typ| Idx]) :- hyp_typ(Typ), nat_idx(Idx).
 
 :- modeb(3, name_equal(+string, +string)).
 :- modeb(3, dif(+string, +string)).
 :- modeb(3, dif(+hyp_idx, +hyp_idx)).
 :- modeb(3, dif(+goal_idx, +goal_idx)).
-:- modeb(3, position_left(+goal_idx, +goal_idx)).
-:- modeb(3, position_left(+hyp_idx, +hyp_idx)).
+:- modeb(3, goal_position_left(+goal_idx, +goal_idx)).
+:- modeb(3, hyp_position_left(+hyp_idx, +hyp_idx)).
 :- modeb(3, position_above(+goal_idx, +goal_idx)).
 :- modeb(3, position_above(+hyp_idx, +hyp_idx)).
 
-:- determination(tac/1, position_left/2).
+:- determination(tac/1, goal_position_left/2).
+:- determination(tac/1, hyp_position_left/2).
 :- determination(tac/1, position_above/2).
 
 :- set(construct_bottom, false).
