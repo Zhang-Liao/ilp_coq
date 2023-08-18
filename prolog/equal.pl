44fbfd21090baff2ae01fff91a614865f53efc63
Zhang-Liao <zhangliao714@gmail.com>
2023-08-18 13:22:55 +0800

    euqal2

--- prolog/equal.pl
+++ prolog/equal.pl
@@ -20,26 +20,33 @@ goal_idx(Idx) :- nat_idx(Idx).
 
 position_above(X, Y) :- dif(X,Y), prefix(X, Y).
 
-succ(Idx, N, (P, RelativeIdx)) :-
+% succ(Idx, N, (P, RelativeIdx)) :-
+%     modeb(P), Fact =.. [P, N, NodeIdx], Fact,
+%     prefix(Idx, NodeIdx),
+%     append(Idx, RelativeIdx, NodeIdx).
+
+% subterm(Idx, N, SubTerm) :-
+%     goal_idx(Idx), !,
+%     findall(Node,
+%         (print('find node'), nl, succ(Idx, N, Node)),
+%         Nodes),
+%     Nodes \== [],
+%     % print(Nodes),
+%     sort(2, @<, Nodes, SubTerm).
+
+node(Idx, N, P, RelativeIdx) :-
     modeb(P), Fact =.. [P, N, NodeIdx], Fact,
     prefix(Idx, NodeIdx),
     append(Idx, RelativeIdx, NodeIdx).
 
-subterm(Idx, N, SubTerm) :-
-    goal_idx(Idx), !,
-    findall(Node,
-        succ(Idx, N, Node),
-        Nodes),
-    Nodes \== [],
-    % print(Nodes),
-    sort(2, @<, Nodes, SubTerm).
-
 eq_subterm(Idx1, Idx2, N) :-
     dif(Idx1, Idx2),
-    subterm(Idx1, N, T1),
-    subterm(Idx2, N, T2),
-    T1 = T2.
-
+    forall(
+        node(Idx1, N, Predc, RelativeIdx1),
+        node(Idx2, N, Predc, RelativeIdx1)),
+    forall(
+        node(Idx2, N, Predc, RelativeIdx2),
+        node(Idx1, N, Predc, RelativeIdx2)).
 % subterm([0], 1).
-% eq_subterm([0], [1], 1).
-% eq_subterm([0], [2], 1).
+% eq_subterm([0], [1], 1). true
+% eq_subterm([0], [2], 1). false
