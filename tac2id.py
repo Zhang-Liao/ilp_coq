628d4cd60d53d8eec5e9c2d8d53b0da75fa582d4
Zhang-Liao <zhangliao714@gmail.com>
2023-08-28 23:04:43 +0800

    tac to id

---
+++ tac2id.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from lib import utils
+
+import json
+
+def build(dir):
+    tacs = set()
+    for root, _, _ in os.walk(dir):
+        for i in range(10):
+            f = os.path.join(root, 'split'+str(i)+'.json')
+            with open(f, 'r') as reader:
+                for l in reader:
+                    l = l.strip()
+                    if utils.not_lemma(l):
+                        l = json.loads(l)
+                        tacs.add(l['tac'])
+        break
+    ids = list(range(0, len(tacs)))
+    tac_ids = zip(list(tacs), ids)
+    return dict(tac_ids)
+    # print(tac_map)
+
+dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split'
+tac_dict = build(dir)
+w = open(os.path.join(dir, 'tac2id.json'), 'w') 
+json.dump(tac_dict, w)
