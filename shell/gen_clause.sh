e83047892aad6f776321817b0a848ade944c035d
Zhang-Liao <zhangliao714@gmail..com>
2023-09-03 08:32:51 +0000

    ignore certain file

--- shell/gen_clause.sh
+++ shell/gen_clause.sh
@@ -4,15 +4,17 @@ loop_gen() {
   file_base_end=$(($file_len - 3))
   file_base=${0:0:file_base_end}
   rule_file="${file_base}_rule.pl"
-  while [ $complete == 0 ]; do
-    swipl $0 >/dev/null
-    complete=$(python all_clause_generated.py --file "${rule_file}")
-  done
+  if [[ ! $0 =~ "18259c0.pl" ]]; then
+    ( while [ $complete == 0 ]; do
+      swipl $0 >/dev/null
+      complete=$(python all_clause_generated.py --file "${rule_file}")
+    done )
+  fi
   echo 'finish' $0
 }
 
 export -f loop_gen
-dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/predc_auto
+dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/predc_auto/neg_dynamic
 
 # find $dir -name "*.b" | parallel python init_noise.py --file {}
 find $dir -name "*_rule.pl" | parallel rm {}
