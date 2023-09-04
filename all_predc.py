6aa1b1407a50f13fc40352c57a2c973e252d9443
Zhang-Liao <zhangliao714@gmail..com>
2023-09-04 11:13:02 +0000

    ignore warning

--- all_predc.py
+++ all_predc.py
@@ -1,23 +1,30 @@
 import json
 import os
 import re
-import sys
-sys.path.append(os.path.dirname(sys.path[0]))
 
 from lib import utils
 
 dat_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split'
-out= '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/all_predc.pl'
+out= '/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/all_predc.pl'
 
 def pr_hyp_predc(predc, writer):
     for p in predc:
-        p = utils.to_predc_name(p)
         writer.write(f"{p}(-1, none, []).\n")
 
 def pr_goal_predc(predc, writer):
     for p in predc:
-        p = utils.to_predc_name(p)
         writer.write(f"{p}(-1, []).\n")
+
+def pr_multifiles(hyp_predc, goal_predc, w):
+    w.write(f":- multifile hyp_predc/1.\n")
+    w.write(f":- multifile goal_predc/1.\n")
+    w.write(f":- multifile hyp_coq_var/4.\n")
+    w.write(f":- multifile goal_coq_var/3.\n")
+    
+    for p in hyp_predc:
+        w.write(f":- multifile {p}/3.\n")
+    for p in goal_predc:
+        w.write(f":- multifile {p}/2.\n")
 
 def file_predc(file, hyp_predc, goal_predc):
     with (open(file, 'r') as reader):
@@ -36,11 +43,14 @@ def read_predc():
         if re.match('split[0-9].json', filename):
             file = os.path.join(dat_dir, filename)
             hyp_predc, goal_predc = file_predc(file, hyp_predc, goal_predc)
+    goal_predc = [utils.to_predc_name(p) for p in goal_predc]
+    hyp_predc = [utils.to_predc_name(p) for p in hyp_predc]
     return hyp_predc, goal_predc
 
 def pr_predc(hyp, goal):
     with open(out, 'a') as w:
-        w.write(':-style_check(-discontiguous).\n')
+        # w.write(':-style_check(-discontiguous).\n')
+        pr_multifiles(hyp, goal, w)
         pr_hyp_predc(hyp, w)
         pr_goal_predc(goal, w)
 
--- shell/gen_clause.sh
+++ shell/gen_clause.sh
@@ -4,7 +4,7 @@ loop_gen() {
   file_base_end=$(($file_len - 3))
   file_base=${0:0:file_base_end}
   rule_file="${file_base}_rule.pl"
-  if [[ ! $0 =~ "18259c0.pl" ]]; then
+  if [[ ! $0 =~ "18259" ]]; then
     ( while [ $complete == 0 ]; do
       swipl $0 >/dev/null
       complete=$(python all_clause_generated.py --file "${rule_file}")
@@ -14,10 +14,11 @@ loop_gen() {
 }
 
 export -f loop_gen
-dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/predc_auto/neg_dynamic
+dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/predc_auto/no_cluster2
 
 # find $dir -name "*.b" | parallel python init_noise.py --file {}
-find $dir -name "*_rule.pl" | parallel rm {}
-find $dir -name "*.pl" | parallel -j 40 bash -c loop_gen
-find $dir -name "*_rule.pl" | xargs -i cat {} > tmp
+# find $dir -name "*_rule.pl" | parallel rm {}
+# find $dir -name "*.pl" | parallel -j 40 bash -c loop_gen
+echo ":- style_check(-singleton).\n" > tmp
+find $dir -name "*_rule.pl" | xargs -i cat {} >> tmp
 mv tmp $dir/alltac_rule.pl
