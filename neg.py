388b307ae9b5a33201e928838504f6f29c9bb03e
Zhang-Liao <zhangliao714@gmail..com>
2023-09-07 07:43:45 +0000

    neg shell

--- neg.py
+++ neg.py
@@ -1,35 +1,35 @@
 import os
-from lib import utils
 import json
+
+import argparse
 import joblib
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MultiLabelBinarizer
 
-feat_encoder = joblib.load('/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split/feat_encoder.gz')
-label_encoder = joblib.load('/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split/label_encoder.gz')
-feat_file = "/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split/1000.feat"
-label_file = "/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/ten_split/1000.label"
+from lib import utils
+
+
+feat_encoder = joblib.load('/home/zhangliao/ilp_out_coq/ilp_out_coq/data/feat_encoder.gz')
+label_encoder = joblib.load('/home/zhangliao/ilp_out_coq/ilp_out_coq/data/label_encoder.gz')
 
 assert(isinstance(label_encoder, LabelEncoder))
 assert(isinstance(feat_encoder, MultiLabelBinarizer))
 
-root, ext = os.path.splitext(feat_file)
-
 print('number of feat', len(feat_encoder.classes_))
 print('number of label', len(label_encoder.classes_))
 
-def read():
+def read(f_feat, f_label):
     labels = []
     feats = []
-    with open(label_file, 'r') as reader:
+    with open(f_label, 'r') as reader:
         for l in reader:
             l = l.strip()
             if utils.not_lemma(l):
                 labels.append(l)
         labels = label_encoder.transform(labels)
 
-    with open(feat_file, 'r') as reader:
+    with open(f_feat, 'r') as reader:
         for l in reader:
             l = l.strip()
             if utils.not_lemma(l):
@@ -46,7 +46,7 @@ def get_negs(ids, labels, label):
         if label2 != label:
             negs.append(int(id))
             n += 1
-            if n == 10:
+            if n == 32:
                 break
     return negs
 
@@ -56,9 +56,9 @@ def update_dic(i, tac, negs, dict):
     else:
         dict[tac][i] = negs
 
-def exg_row_ids_map():
+def exg_row_ids_map(f_label):
     mp = []
-    with open(label_file, 'r') as reader:
+    with open(f_label, 'r') as reader:
         row_id = 0
         for l in reader:
             l = l.strip()
@@ -76,9 +76,16 @@ def map_exg_row_ids(dict, mp):
         new_dict[tac] = new_pos_negs
     return new_dict
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--feat", type=str)
+parser.add_argument("--label", type=str)
+
+opts = parser.parse_args()
+root, ext = os.path.splitext(opts.label)
+
 dict = {}
-feats, labels = read()
-neigh = KNeighborsClassifier(n_neighbors=1, metric='jaccard', n_jobs=5)
+feats, labels = read(opts.feat, opts.label)
+neigh = KNeighborsClassifier(n_neighbors=1, metric='jaccard', n_jobs=10)
 neigh.fit(feats, labels)
 kneighs_arr = neigh.kneighbors(feats, 500, False)
 for i in range(len(labels)):
@@ -88,10 +95,10 @@ for i in range(len(labels)):
     negs = get_negs(kneighs, k_labels, labels[i])
     update_dic(i, tac, negs, dict)
 
-out = root + "_neg2.json"
+out = root + "_neg.json"
 
-mp = exg_row_ids_map()
+mp = exg_row_ids_map(opts.label)
 dict = map_exg_row_ids(dict, mp)
-print(dict)
+# print(dict)
 with open(out, 'w') as w:
     json.dump(dict, w)
---
+++ shell/neg.sh
@@ -0,0 +1,6 @@
+dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/rand_train_test
+
+for i in {0..19}
+do
+    (python neg.py --feat $dir/train$i.feat --label $dir/train$i.label)&
+done
