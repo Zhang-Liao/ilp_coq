71d7cba27f7f1a27ba24e919b65922b221ef1e87
Zhang-Liao <zhangliao714@gmail..com>
2023-09-07 07:44:26 +0000

    prop

---
+++ all_prop_predc.py
@@ -0,0 +1,56 @@
+import json
+import os
+import re
+
+from lib import utils
+
+dat_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split'
+out= '/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/all_prop_predc.pl'
+
+def pr_hyp_predc(predc, writer):
+    for p in predc:
+        writer.write(f"{p}(-1, none, []).\n")
+
+def pr_goal_predc(predc, writer):
+    for p in predc:
+        writer.write(f"{p}(-1, []).\n")
+
+def pr_multifiles(hyp_predc, goal_predc, w):
+    for p in hyp_predc:
+        w.write(f":- multifile {p}/3.\n")
+    for p in goal_predc:
+        w.write(f":- multifile {p}/2.\n")
+
+def file_predc(file, hyp_predc, goal_predc):
+    with (open(file, 'r') as reader):
+        for l in reader:
+            l = l.strip()
+            if utils.not_lemma(l):
+                l = json.loads(l)
+                hyp_predc = utils.add_hyps_prop_predc(l['hyps'], hyp_predc)
+                goal_predc = utils.add_goal_prop_predc(l['goal'], goal_predc)
+    return hyp_predc, goal_predc
+
+def read_predc():
+    hyp_predc = set()
+    goal_predc = set()
+    for filename in os.listdir(dat_dir):
+        if re.match('split[0-9].json', filename):
+            file = os.path.join(dat_dir, filename)
+            hyp_predc, goal_predc = file_predc(file, hyp_predc, goal_predc)
+    goal_predc = [utils.to_predc_name(p) for p in goal_predc]
+    hyp_predc = [utils.to_predc_name(p) for p in hyp_predc]
+    return hyp_predc, goal_predc
+
+def pr_predc(hyp, goal):
+    with open(out, 'a') as w:
+        # w.write(':-style_check(-discontiguous).\n')
+        pr_multifiles(hyp, goal, w)
+        pr_hyp_predc(hyp, w)
+        pr_goal_predc(goal, w)
+
+if os.path.exists(out):
+    os.remove(out)
+
+hyp_predc, goal_predc = read_predc()
+pr_predc(hyp_predc, goal_predc)
---
+++ prop_test_example_predc.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+from lib import utils
+
+test = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8.json'
+out_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8/test_predc/prop'
+
+def init_out(i):
+    out = os.path.join(out_dir, f'{i}.pl')
+    if os.path.exists(out):
+        os.remove(out)
+    return out
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+with open(test, 'r') as r:
+    row_i = 0
+    for l in r:
+        l = l.strip()
+        if utils.not_lemma(l):
+            l = json.loads(l)
+            out = init_out(row_i)
+            with open(out, 'a') as w:
+                w.write(':-style_check(-discontiguous).\n')
+                utils.pr_goal_prop_predc(row_i, l['goal'], w)
+                goal_predc = utils.add_goal_prop_predc(l['goal'], set())
+                utils.pr_hyps_prop_predc(row_i, l['hyps'], w)
+                hyp_predc = utils.add_hyps_prop_predc(l['hyps'], set())
+                for p in goal_predc:
+                    w.write(f"goal_predc({p}).\n")
+                for p in hyp_predc:
+                    w.write(f"hyp_predc({p}).\n")
+            
+        row_i += 1
+
+
