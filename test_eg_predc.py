49660d61de4ff090307dfc0d4ac298d96f23114c
Zhang-Liao <zhangliao714@gmail..com>
2023-09-08 11:41:53 +0000

    parallel test exg predc

--- test_eg_predc.py
+++ test_eg_predc.py
@@ -1,27 +1,31 @@
 import json
 import os
 
+import argparse
+
 from lib import utils
 
-test = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8.json'
-out_dir = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/ten_split/split8/test_predc'
-
-def init_out(i):
+def init_out(i, out_dir):
     out = os.path.join(out_dir, f'{i}.pl')
     if os.path.exists(out):
         os.remove(out)
     return out
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", type=str)
+parser.add_argument("--out", type=str)
+opts = parser.parse_args()
 
-with open(test, 'r') as r:
+if not os.path.exists(opts.out):
+    os.makedirs(opts.out)
+
+with open(opts.test, 'r') as r:
     row_i = 0
     for l in r:
         l = l.strip()
         if utils.not_lemma(l):
             l = json.loads(l)
-            out = init_out(row_i)
+            out = init_out(row_i, opts.out)
             with open(out, 'a') as w:
                 w.write(':-style_check(-discontiguous).\n')
                 utils.pr_goal_predc(row_i, l['goal'], w)
@@ -34,5 +38,4 @@ with open(test, 'r') as r:
                     w.write(f"hyp_predc({p}).\n")
             
         row_i += 1
-
 
