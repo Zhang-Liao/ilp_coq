43e9737554daf025959c54718f17c3ed7966c6e7
Zhang-Liao <zhangliao714@gmail.com>
2023-09-12 22:52:24 +0200

    sort all tacs

--- stats/all_tacs.py
+++ stats/all_tacs.py
@@ -1,21 +1,27 @@
+import json
 import os
 import sys
-from pathlib import Path
+
 sys.path.append(os.path.dirname(sys.path[0]))
 from lib import utils
 
 labels = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/origin_feat/ten_split'
-out = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/all.label'
+out = '/home/zhangliao/ilp_out_coq/ilp_out_coq/data/all_tac.json'
 
-tacs = set()
+tacs = {}
 for i in range(10):
     file = os.path.join(labels, f'split{i}.label')
     with open(file, 'r') as r :
         for t in r:
-            if utils.notlemma(t):
-                tacs.add(t.strip())
+            if utils.not_lemma(t):
+                t = t.strip()
+                if t not in tacs.keys():
+                    tacs[t] = 0
+                else:
+                    tacs[t] += 1
     # print(tacs)
 
+tacs = dict(sorted(tacs.items(), key = lambda x : x[1], reverse = True))
+
 with open(out, 'w') as w:
-    for t in tacs:
-        w.write(t + '\n')
+    json.dump(tacs, w, indent=4)
--- stats/stat_filter.py
+++ stats/stat_filter.py
@@ -10,37 +10,41 @@ import scipy.stats as stats
 sys.path.append(os.path.dirname(sys.path[0]))
 from lib import utils
 
+
 def round_f(acc):
-    return round (acc * 100, 5)
+    return round(acc * 100, 5)
+
 
 def load(f_good, f_label, f_pred):
-    with open(f_label, 'r') as f:
+    with open(f_label, "r") as f:
         labels = f.read().splitlines()
     labels = [l.strip() for l in labels]
 
-    with open(f_good, 'r') as f:
+    with open(f_good, "r") as f:
         goodss = f.read().splitlines()
-    goodss = [l.strip().split('\t') for l in goodss]
+    goodss = [l.strip().split("\t") for l in goodss]
 
-    with open(f_pred, 'r') as f:
+    with open(f_pred, "r") as f:
         predss = f.read().splitlines()
-    predss = [l.strip().split('\t') for l in predss]
+    predss = [l.strip().split("\t") for l in predss]
 
     assert len(goodss) == len(labels)
-    return labels, goodss, predss    
+    return labels, goodss, predss
+
 
 def neg_predict_value(dict):
     for tac, stat in dict.items():
-        neg = stat['TN'] + stat['FN']
-        all = neg + stat['TP'] + stat['FP']
-        
+        neg = stat["TN"] + stat["FN"]
+        all = neg + stat["TP"] + stat["FP"]
+
         if neg == 0:
-            dict[tac]['npv'] = 1
+            dict[tac]["npv"] = 1
         else:
-            dict[tac]['npv'] = stat['TN'] / (stat['TN'] + stat['FN'])
-        
-        dict[tac]['tn_div_all'] = stat['TN'] / all 
+            dict[tac]["npv"] = stat["TN"] / (stat["TN"] + stat["FN"])
+
+        dict[tac]["tn_div_all"] = stat["TN"] / all
     return dict
+
 
 def stat(f_good, f_label, f_pred):
     labels, goodss, predss = load(f_good, f_label, f_pred)
@@ -51,54 +55,46 @@ def stat(f_good, f_label, f_pred):
             preds = preds[:10]
             for p in preds:
                 if p not in tac_stats.keys():
-                    tac_stats[p] = {'num': 1, 'TP' : 0, 'TN' : 0, 'FP' : 0, 'FN' : 0}
+                    tac_stats[p] = {"num": 1, "TP": 0, "TN": 0, "FP": 0, "FN": 0}
                 else:
-                    tac_stats[p]['num'] += 1
-                    
+                    tac_stats[p]["num"] += 1
+
                 is_good = p in goods
-                is_correct = (p == label)
+                is_correct = p == label
                 if is_good & is_correct:
-                    tac_stats[p]['TP'] += 1
+                    tac_stats[p]["TP"] += 1
                 elif is_good & (not is_correct):
-                    tac_stats[p]['FP'] += 1
+                    tac_stats[p]["FP"] += 1
                 elif (not is_good) & (not is_correct):
-                    tac_stats[p]['TN'] += 1
+                    tac_stats[p]["TN"] += 1
                 else:
-                    tac_stats[p]['FN'] += 1    
-            # if label in preds:
-            #     n_in_pred += 1
-            #     if label not in goods:
-            #         false_neg += 1
-            # bad = [g != label for g in goods]
-            # false_pos += len(bad)
+                    tac_stats[p]["FN"] += 1
 
     items = tac_stats.items()
-    tac_stats = dict(sorted(items, key=lambda x:x[1]['num'], reverse= True))
+    tac_stats = dict(sorted(items, key=lambda x: x[1]["num"], reverse=True))
     tac_stats = neg_predict_value(tac_stats)
     stats = tac_stats.values()
-    
-    truth_neg = sum([s['TN'] for s in stats])
-    false_neg = sum([s['FN'] for s in stats])
-    all = sum([s['TN'] + s['TP'] + s['FN'] + s['FP'] for s in stats])
+
+    truth_neg = sum([s["TN"] for s in stats])
+    false_neg = sum([s["FN"] for s in stats])
+    all = sum([s["TN"] + s["TP"] + s["FN"] + s["FP"] for s in stats])
     neg_pred_v = truth_neg / (truth_neg + false_neg)
     tn_div_all = truth_neg / all
-    
-    log = {
-        'npv' : neg_pred_v,
-        'tn_div_all' : tn_div_all
-    }
+
+    log = {"npv": neg_pred_v, "tn_div_all": tn_div_all}
     log = log | tac_stats
     # for tac, stat in tac_stats.ite
     out_dir = os.path.dirname(f_good)
-    with open(os.path.join(out_dir, 'stat_filter.json'), 'w') as w:
+    with open(os.path.join(out_dir, "stat_filter.json"), "w") as w:
         json.dump(log, w, indent=4)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-    description="Returns false positive and false negative.")
-    parser.add_argument('--good', type=str)
-    parser.add_argument('--pred', type=str)
-    parser.add_argument('--label', type=str)
+        description="Returns false positive and false negative."
+    )
+    parser.add_argument("--good", type=str)
+    parser.add_argument("--pred", type=str)
+    parser.add_argument("--label", type=str)
     args = parser.parse_args()
     stat(args.good, args.label, args.pred)
