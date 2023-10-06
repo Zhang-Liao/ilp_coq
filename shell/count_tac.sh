0d0400a1c68011e32782afd93f9498aca8aa7371
Zhang-Liao <zhangliao714@gmail..com>
2023-10-06 15:39:24 +0000

    count tac

---
+++ shell/count_tac.sh
@@ -0,0 +1,22 @@
+dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/neg/theories/Structures
+
+echo "number of proof states"
+grep -r -v "^#lemma" $dir | wc -l
+
+echo intros
+grep -r "\"intros\"" $dir | wc -l
+
+echo simpl
+grep -r "\"simpl\"" $dir | wc -l
+
+echo auto
+grep -r "\"auto\"" $dir | wc -l
+
+echo reflexivity
+grep -r "\"reflexivity\"" $dir | wc -l
+
+echo assumption
+grep -r "\"assumption\"" $dir | wc -l
+
+echo trivial
+grep -r "\"trivial\"" $dir | wc -l
