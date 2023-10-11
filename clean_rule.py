1c3237da3b423a54c350ae0f1cad46c89c6a18f8
Zhang-Liao <zhangliao714@gmail..com>
2023-10-11 14:50:09 +0000

    subsumption

--- clean_rule.py
+++ clean_rule.py
@@ -37,6 +37,36 @@ def sort_rules(dct):
     return dct
 
 
+def sep_clauses(clauses):
+    sep_clss = []
+    for c in clauses:
+        predcs = c.split("),")
+        last = predcs[-1][:-1]
+        other = [p + ")" for p in predcs[:-1]]
+        sep_c = other + [last]
+        sep_c = [p.strip() for p in sep_c]
+        sep_clss.append(sep_c)
+    return sep_clss
+
+
+def too_general(rule, other_rules):
+    return any(set(rule).issubset(set(orule)) for orule in other_rules)
+    # for orule in other_rules:
+    #     if rule.isusbset(orule):
+
+
+def rmv_too_general(dict):
+    for tac, rules in dict.items():
+        new_rules = []
+        for i in range(len(rules)):
+            r = rules[i]
+            other = rules[:i] + rules[i + 1 :]
+            if not too_general(r, other):
+                new_rules.append((", ".join(r)) + ".")
+        dict[tac] = new_rules
+    return dict
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", type=str)
 
@@ -51,6 +81,9 @@ with open(opts.file, "r") as r:
 assert dat[0] == ":- style_check(-singleton).\n"
 dat = dat[1:]
 rule_dic = sort_rules(mk_rule_dic(dat))
+rule_dic = {k: sep_clauses(v) for k, v in rule_dic.items()}
+rule_dic = rmv_too_general(rule_dic)
+
 
 with open(opts.file, "w") as w:
     w.write(":- style_check(-singleton).\n")
