a256c4829ddf3e3106cc77c4331c37fb9406c35a
Zhang-Liao <zhangliao714@gmail..com>
2023-11-02 16:09:49 +0000

    split test stat

--- stats/gather_rand_split_test.py
+++ stats/gather_rand_split_test.py
@@ -14,8 +14,6 @@ def init_stat():
     stat = {}
     for kind in predc_kinds:
         stat[kind] = {}
-        for t in utils.THEORIES:
-            stat[kind][t] = {}
     return stat
 
 
@@ -27,7 +25,7 @@ def check_miss(stats):
                     print("miss: pos", pos, "neg", neg, "split", split)
 
 
-def update_stat(i, stat, file, root, theory):
+def update_stat(i, stat, file, root):
     reader = open(os.path.join(root, file), "r")
     file_stat = json.load(reader)
     f1 = file_stat["f1"]
@@ -36,17 +34,17 @@ def update_stat(i, stat, file, root, theory):
     splits = root.split("/")
     if ("anonym" in splits) & ("rel" in splits):
         try:
-            stat["f1"]["anonym_rel"][theory][i] = f1
-            stat["f1_no_ignored_tac"]["anonym_rel"][theory][i] = f1_no_ign
+            stat["f1"]["anonym_rel"][i] = f1
+            stat["f1_no_ignored_tac"]["anonym_rel"][i] = f1_no_ign
         except:
             print("fail in", os.path.join(root, file))
             exit()
     elif ("origin" in splits) & ("rel" in splits):
-        stat["f1"]["origin_rel"][theory][i] = f1
-        stat["f1_no_ignored_tac"]["origin_rel"][theory][i] = f1_no_ign
+        stat["f1"]["origin_rel"][i] = f1
+        stat["f1_no_ignored_tac"]["origin_rel"][i] = f1_no_ign
     elif ("origin" in splits) & ("prop" in splits):
-        stat["f1"]["origin_prop"][theory][i] = f1
-        stat["f1_no_ignored_tac"]["origin_prop"][theory][i] = f1_no_ign
+        stat["f1"]["origin_prop"][i] = f1
+        stat["f1_no_ignored_tac"]["origin_prop"][i] = f1_no_ign
     else:
         raise FileNotFoundError("stat_filter exist in unexpected path")
 
@@ -55,7 +53,7 @@ def update_stats(dir, i, stat):
     for root, _, files in os.walk(dir):
         for f in files:
             if f.endswith("stat_filter.json"):
-                f1 = update_stat(i, stat, f, root, theory)
+                f1 = update_stat(i, stat, f, root)
 
 
 stat = {"acc": init_stat(), "f1": init_stat(), "f1_no_ignored_tac": init_stat()}
