61fb6b576435ef3034338104ca95e647b564137d
Zhang-Liao <zhangliao714@gmail..com>
2023-11-08 08:27:08 +0000

    gen theory clause

---
+++ shell/gen_theory_rel_clause.sh
@@ -0,0 +1,28 @@
+gen() {
+  swipl $0 >/dev/null
+  echo 'finish' $0
+}
+
+export -f gen
+
+negs=(1 2 4 8 16 32)
+poss=(2 4 8 16 32)
+
+anonym=origin
+kind=rel
+for neg in "${negs[@]}"; do
+  (
+
+    for pos in "${poss[@]}"; do
+      dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
+      cd $dir
+      find $dir -name "*_rule.pl" | parallel rm {}
+      time find $dir -name "*.pl" | parallel --timeout 10m -j 10 bash -c gen
+      echo ":- style_check(-singleton)." >tmp
+      find . -name "*_rule.pl" | xargs -i cat {} >>tmp
+      cd -
+      python clean_rule.py --file $dir/tmp
+      mv $dir/tmp $dir/alltac_rule.pl
+    done
+  ) &
+done
