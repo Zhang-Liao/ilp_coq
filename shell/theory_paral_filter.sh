b282ae7a15ae6c56997ab6533a5085f7a11d9794
Zhang-Liao <zhangliao714@gmail..com>
2023-11-08 16:06:00 +0000

    theory parallel filter

---
+++ shell/theory_paral_filter.sh
@@ -0,0 +1,15 @@
+pred=data/json/origin_feat/tune/QArith/test_theory/theories/Lists
+all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym_predc.pl
+negs=(1 2 4 8 16 32)
+poss=(2 4 8 16 32)
+anonym=anonym
+kind=rel
+test=data/json/predicate/$anonym/merge/test/theories/Lists
+for neg in "${negs[@]}"; do
+    for pos in "${poss[@]}"; do
+        (
+            dir=data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
+            python filter.py --clause $dir/alltac_rule.pl --pred $pred.eval --test $test --label $pred.label --all_predc $all_predc --info $kind/$anonym/p$pos\n$neg
+        ) &
+    done
+done
