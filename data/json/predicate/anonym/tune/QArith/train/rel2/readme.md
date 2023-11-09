eeb802c5c3aa51f76db5ad7f30fed4992633b2a0
Zhang-Liao <zhangliao714@gmail..com>
2023-11-09 18:05:09 +0000

    tune in theories

---
+++ data/json/predicate/anonym/tune/QArith/train/rel2/readme.md
@@ -0,0 +1,3 @@
+is_goal_root([]).
+:- modeb(20, dif(+string, +string)).
+:- set(clauselength, 100).
--- shell/tune_rel_theory.sh
+++ shell/tune_rel_theory.sh
@@ -1,16 +1,24 @@
-pred=data/json/origin_feat/tune/QArith/test_theory/theories/Lists.eval
-label=data/json/origin_feat/merge/theories/Lists.label
-all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/all_predc.pl
+knn_pred=data/json/origin_feat/tune/QArith/test_theory
+all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/all_prop_predc.pl
 negs=(1 2 4 8 16 32)
 poss=(2 4 8 16 32)
 anonym=origin
 kind=rel
-test=data/json/predicate/$anonym/merge/test/theories/Lists
-for neg in "${negs[@]}"; do
-    for pos in "${poss[@]}"; do
+test=data/json/predicate/$anonym/merge/test/
+# theories=('theories/Classes' 'theories/Lists' 'theories/Numbers')
+theories=('theories/Sorting' 'theories/Numbers')
+for theory in "${theories[@]}"; do
+    for neg in "${negs[@]}"; do
         (
-            dir=data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
-            python filter.py --clause $dir/alltac_rule.pl --pred $pred --test $test --label $label --all_predc $all_predc --info $kind/$anonym/p$pos\n$neg
+            for pos in "${poss[@]}"; do
+                dir=data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
+                python filter.py --clause $dir/alltac_rule.pl \
+                    --pred $knn_pred/$theory.eval \
+                    --test $test/$theory \
+                    --label data/json/origin_feat/merge/$theory.label \
+                    --all_predc $all_predc \
+                    --info $kind/$anonym/p$pos\n$neg
+            done
         ) &
     done
 done
