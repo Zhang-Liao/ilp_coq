0f8c583c030f6c6031246e41ad18ca829456e7c4
Zhang-Liao <zhangliao714@gmail..com>
2023-11-10 16:10:32 +0000

    gather theory tune

--- stats/gather_theory_tune.py
+++ stats/gather_theory_tune.py
@@ -3,6 +3,8 @@ import os
 import re
 import sys
 
+import numpy as np
+
 sys.path.append(os.path.dirname(sys.path[0]))
 from lib import utils
 
@@ -12,20 +14,32 @@ num_of_test = 10
 # anonym = "origin"
 # predc_kind = ""
 # assert anonym in ["origin", "anonym"]
-predc_kinds = ["anonym_rel", "origin_prop", "origin_rel"]
+KINDS = ["anonym_rel", "origin_prop", "origin_rel"]
+POS = [2, 4, 8, 16, 32]
+NEG = [1, 2, 4, 8, 16, 32]
 
 
-def init_stat(theory):
+def init_stat(theories):
     stat = {}
-    pos = [2, 4, 8, 16, 32]
-    neg = [1, 2, 4, 8, 16, 32]
-    for kind in predc_kinds:
+    for kind in KINDS:
         stat[kind] = {}
-        stat[kind][theory] = {}
-        for p in pos:
-            stat[kind][theory][p] = {}
-            for n in neg:
-                stat[kind][theory][p][n] = {}
+        for theory in theories:
+            stat[kind][theory] = {}
+            for p in POS:
+                stat[kind][theory][p] = {}
+                for n in NEG:
+                    stat[kind][theory][p][n] = {}
+    return stat
+
+
+def init_aver_f1():
+    stat = {}
+    for kind in KINDS:
+        stat[kind] = {}
+        for p in POS:
+            stat[kind][p] = {}
+            for n in NEG:
+                stat[kind][p][n] = []
     return stat
 
 
@@ -44,10 +58,10 @@ def update_theory_stat(stat, ilp_stat_f, root, theory, pos, neg):
     reorder_reader = open(reorder_f, "r")
 
     file_stat = json.load(ilp_reader)
-    reorder_stat = json.load(reorder_reader)
+    # reorder_stat = json.load(reorder_reader)
     f1 = file_stat["f1"]
     f1_no_ign = file_stat["f1_no_ignored_tac"]
-    acc = reorder_stat["accs"]
+    # acc = reorder_stat["accs"]
     # print(f1)
     splits = root.split("/")
     if ("anonym" in splits) & ("rel" in splits):
@@ -89,19 +103,42 @@ def update_knn(dir, theory, stat):
     stat[theory] = knn_stat["accs"]
 
 
-theory = "theories/Lists"
-ilp_stat = {
-    "acc": init_stat(theory),
-    "f1": init_stat(theory),
-    "f1_no_ignored_tac": init_stat(theory),
-}
-knn_stat = init_knn_stat(theory)
-dir = f"data/json/origin_feat/tune/QArith/test_theory/{theory}"
-# dir = os.path.join(stat_i, theory)
-# update_knn(dir, theory, knn_stat)
-update_theory_stats(dir, ilp_stat, theory)
+def cal_aver_f1(stat, key):
+    aver_f1 = init_aver_f1()
+    for kind, kind_stat in stat[key].items():
+        for theory_stat in kind_stat.values():
+            for pos, pos_stat in theory_stat.items():
+                for neg, f1 in pos_stat.items():
+                    aver_f1[kind][pos][neg].append(f1)
 
-with open("List_stat_no_split.json", "w") as w:
+    for kind, kind_stat in aver_f1.items():
+        for pos, pos_stat in kind_stat.items():
+            for neg, f1s in pos_stat.items():
+                aver_f1[kind][pos][neg] = np.average(f1s)
+
+    for kind in stat[key].keys():
+        stat[key][kind]["aver"] = aver_f1[kind]
+
+
+theories = ["theories/Lists", "theories/Sorting"]
+ilp_stat = {
+    "acc": init_stat(theories),
+    "f1": init_stat(theories),
+    "f1_no_ignored_tac": init_stat(theories),
+}
+# print(ilp_stat)
+for theory in theories:
+    knn_stat = init_knn_stat(theory)
+    dir = f"data/json/origin_feat/tune/QArith/test_theory/{theory}"
+    # dir = os.path.join(stat_i, theory)
+    # update_knn(dir, theory, knn_stat)
+    update_theory_stats(dir, ilp_stat, theory)
+
+
+# cal_aver_f1(ilp_stat, "f1")
+# cal_aver_f1(ilp_stat, "f1_no_ignored_tac")
+
+with open("tune_QArith.json", "w") as w:
     json.dump(ilp_stat, w)
 
 # with open("knn_List.json", "w") as w:
