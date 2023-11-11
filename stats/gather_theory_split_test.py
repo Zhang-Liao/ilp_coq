d59038da3b2f173312a30d7b9b8efab081663a69
Zhang-Liao <zhangliao714@gmail..com>
2023-11-11 08:38:48 +0000

    gather theory

--- stats/gather_theory_split_test.py
+++ stats/gather_theory_split_test.py
@@ -13,11 +13,6 @@ num_of_test = 10
 # predc_kind = ""
 # assert anonym in ["origin", "anonym"]
 predc_kinds = ["anonym_rel", "origin_prop", "origin_rel"]
-params = {
-    "anonym_rel": "p32n16",
-    "origin_prop": "p2n1",
-    "origin_rel": "p4n2",
-}
 
 
 def init_stat():
@@ -36,7 +31,7 @@ def init_knn_stat():
     return stat
 
 
-def update_theory_stat(stat, ilp_stat_f, root, theory):
+def update_theory_stat(i, stat, ilp_stat_f, root, theory):
     theory_name = theory.split("/")[-1]
     path_splits = root.split("/")
     test_dir = "/".join(path_splits[:-1])
@@ -52,50 +47,48 @@ def update_theory_stat(stat, ilp_stat_f, root, theory):
     # print(f1)
     splits = root.split("/")
     if ("anonym" in splits) & ("rel" in splits):
-        if params["anonym_rel"] in splits:
-            stat["f1"]["anonym_rel"][theory] = f1
-            stat["f1_no_ignored_tac"]["anonym_rel"][theory] = f1_no_ign
-            stat["acc"]["anonym_rel"][theory] = acc
+        stat["f1"]["anonym_rel"][theory][i] = f1
+        stat["f1_no_ignored_tac"]["anonym_rel"][theory][i] = f1_no_ign
+        stat["acc"]["anonym_rel"][theory][i] = acc
     elif ("origin" in splits) & ("rel" in splits):
-        if params["origin_rel"] in splits:
-            stat["f1"]["origin_rel"][theory] = f1
-            stat["f1_no_ignored_tac"]["origin_rel"][theory] = f1_no_ign
-            stat["acc"]["origin_rel"][theory] = acc
+        stat["f1"]["origin_rel"][theory][i] = f1
+        stat["f1_no_ignored_tac"]["origin_rel"][theory][i] = f1_no_ign
+        stat["acc"]["origin_rel"][theory][i] = acc
 
     elif ("origin" in splits) & ("prop" in splits):
-        if params["origin_prop"] in splits:
-            stat["f1"]["origin_prop"][theory] = f1
-            stat["f1_no_ignored_tac"]["origin_prop"][theory] = f1_no_ign
-            stat["acc"]["origin_prop"][theory] = acc
+        stat["f1"]["origin_prop"][theory][i] = f1
+        stat["f1_no_ignored_tac"]["origin_prop"][theory][i] = f1_no_ign
+        stat["acc"]["origin_prop"][theory][i] = acc
     else:
         raise FileNotFoundError("stat_filter exist in unexpected path")
 
 
-def update_theory_stats(dir, stat, theory):
+def update_theory_stats(dir, i, stat, theory):
     for root, _, files in os.walk(dir):
         for f in files:
             if f.endswith("stat_filter.json"):
-                update_theory_stat(stat, f, root, theory)
+                update_theory_stat(i, stat, f, root, theory)
 
 
 def update_knn(dir, theory, stat):
     file = os.path.join(dir, f"{theory}_stat.json")
     reader = open(file, "r")
     knn_stat = json.load(reader)
-    stat[theory] = knn_stat["accs"]
+    stat[theory][i] = knn_stat["accs"]
 
 
 ilp_stat = {"acc": init_stat(), "f1": init_stat(), "f1_no_ignored_tac": init_stat()}
 
 knn_stat = init_knn_stat()
-test_dir = f"data/json/origin_feat/tune/QArith/test_theory/"
-for theory in utils.THEORIES:
-    dir = os.path.join(test_dir, theory)
-    # update_knn(stat_i, theory, knn_stat)
-    update_theory_stats(dir, ilp_stat, theory)
+for i in range(num_of_test):
+    stat_i = f"data/json/origin_feat/tune/QArith/test_theory/train{i}"
+    for theory in utils.THEORIES:
+        dir = os.path.join(stat_i, theory)
+        update_knn(stat_i, theory, knn_stat)
+        update_theory_stats(dir, i, ilp_stat, theory)
 
-with open("test_Qarith.json", "w") as w:
+with open("theory_stat.json", "w") as w:
     json.dump(ilp_stat, w)
 
-# with open("knn_theory_stat.json", "w") as w:
-#     json.dump(knn_stat, w)
+with open("knn_theory_stat.json", "w") as w:
+    json.dump(knn_stat, w)
