6ccd05d7f57aa81a69a151d91569f8bbe270633a
Zhang-Liao <zhangliao714@gmail..com>
2023-11-21 22:32:25 +0000

    minor

--- shell/tune_anonym_prop_theory.sh
+++ shell/tune_anonym_prop_theory.sh
@@ -1,24 +1,35 @@
-noarity=_noarity
+# noarity=_noarity
+descript=_ident
 knn_pred=data/json/origin_feat/tune/QArith/test_theory
-all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym$noarity\_predc.pl
+all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym$descript\_predc.pl
 negs=(1 2 4 8 16 32)
 poss=(2 4 8 16 32)
+
 anonym=anonym
-kind=prop$noarity
-test=data/json/predicate/$anonym/merge/test/
+kind=prop_ident
+test=data/json/predicate/$anonym/merge/test$descript/
+# theories=('theories/Sorting' 'theories/NArith' 'theories/Init' 'theories/Vectors' 'plugins/setoid_ring')
 theories=('theories/Lists')
+
 for theory in "${theories[@]}"; do
     for neg in "${negs[@]}"; do
         for pos in "${poss[@]}"; do
             (
-
                 dir=data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
                 python filter.py --clause $dir/alltac_rule.pl \
                     --pred $knn_pred/$theory.eval \
                     --test $test/$theory \
                     --label data/json/origin_feat/merge/$theory.label \
                     --all_predc $all_predc \
-                    --info $kind/$anonym/p$pos\n$neg
+                    --info prop/$anonym$descript/p$pos\n$neg
+
+                # python ilp_pred.py --clause $dir/alltac_rule.pl \
+                #     --out_theory $knn_pred/$theory \
+                #     --test $test/$theory \
+                #     --label data/json/origin_feat/merge/$theory.label \
+                #     --all_predc $all_predc \
+                #     --info $kind/$anonym/p$pos\n$neg
+
             ) &
         done
     done
--- shell/tune_anonym_theory.sh
+++ shell/tune_anonym_theory.sh
@@ -1,26 +1,25 @@
 knn_pred=data/json/origin_feat/tune/QArith/test_theory
-all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym_predc.pl
 negs=(1 2 4 8 16 32)
 poss=(2 4 8 16 32)
+# poss=(4)
 anonym=anonym
+descript=_ident
+all_predc=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym$descript\_predc.pl
 kind=rel
-test=data/json/predicate/$anonym/merge/test/
-# theories=('theories/Classes' 'theories/Lists' 'theories/Numbers')
-theories=('theories/Sorting' 'theories/Numbers')
+test=data/json/predicate/$anonym/merge/test$descript/
+# theories=('theories/Sorting' 'theories/Init' 'plugins/setoid_ring' 'theories/Vectors' 'theories/NArith')
+theories=('theories/Lists')
 for theory in "${theories[@]}"; do
     for neg in "${negs[@]}"; do
         (
-
             for pos in "${poss[@]}"; do
-
-                dir=data/json/predicate/$anonym/tune/QArith/train/$kind/p$pos\n$neg
+                dir=data/json/predicate/$anonym/tune/QArith/train/$kind$descript/p$pos\n$neg
                 python filter.py --clause $dir/alltac_rule.pl \
                     --pred $knn_pred/$theory.eval \
                     --test $test/$theory \
                     --label data/json/origin_feat/merge/$theory.label \
                     --all_predc $all_predc \
-                    --info $kind/$anonym/p$pos\n$neg
-
+                    --info $kind/$anonym$descript/p$pos\n$neg
             done
         ) &
     done
