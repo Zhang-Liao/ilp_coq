bd728642acacd53c26ec92c3d90ec10fd945f5bb
Zhang-Liao <zhangliao714@gmail..com>
2023-11-30 10:26:35 +0000

    clean

--- shell/paral_gen_test_eg.sh
+++
@@ -1,6 +0,0 @@
-dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/predicate/rand_train_test/
-
-for i in {0..19}
-do
-    (python test_eg_predc.py --test $dir/test$i.json --out $dir/test/prop/test$i --prop)&
-done
--- shell/preproc_splits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/origin_feat/tune/MSets
-for i in {0..9}; do
-    (
-        python preproc/preproc.py --file $dir/train$i.json
-        python preproc/preproc.py --file $dir/test$i.json
-    )
-done
