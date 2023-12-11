ddee6b5c50baa883a9376f7f007cc2eff0a95afe
Zhang-Liao <zhangliao714@gmail..com>
2023-12-11 05:50:25 +0000

    clean

--- stats/gather_theory_tune.py
+++ stats/gather_theory_tune.py
@@ -11,7 +11,8 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from lib import utils
 
 num_of_test = 10
-precs = [0, 0.05, 0.10, 0.15, 0.2]
+precs = [0, 0.10, 0.2, 0.3, 0.4, 0.5]
+# precs = [0, 0.05, 0.10, 0.15, 0.2]
 KINDS = ["anonym_rel", "anonym_prop", "origin_prop", "origin_rel"]
 POS = [2, 4, 8, 16, 32]
 NEG = [1, 2, 4, 8, 16, 32]
@@ -78,22 +79,18 @@ def update_theory_stat(stat, ilp_stat_f, root, theory, pos, neg, prec):
     # acc = reorder_stat["accs"]
     # print(f1)
     splits = root.split("/")
-    if ("anonym_id" in splits) & ("rel" in splits):
+    if ("anonym" in splits) & ("rel" in splits):
         stat["f1"]["anonym_rel"][theory][pos][neg][prec] = f1
-        stat["f1_no_ignored_tac"]["anonym_rel"][theory][pos][neg][prec] = f1_no_ign
-        # stat["acc"]["anonym_rel"][theory][pos][neg] = acc
-    elif ("anonym_noarity" in splits) & ("prop" in splits):
+        # stat["f1_no_ignored_tac"]["anonym_rel"][theory][pos][neg][prec] = f1_no_ign
+    elif ("anonym" in splits) & ("prop" in splits):
         stat["f1"]["anonym_prop"][theory][pos][neg][prec] = f1
-        stat["f1_no_ignored_tac"]["anonym_prop"][theory][pos][neg][prec] = f1_no_ign
-        # stat["acc"]["anonym_prop"][theory][pos][neg] = acc
+        # stat["f1_no_ignored_tac"]["anonym_prop"][theory][pos][neg][prec] = f1_no_ign
     elif ("origin" in splits) & ("rel" in splits):
         stat["f1"]["origin_rel"][theory][pos][neg][prec] = f1
-        stat["f1_no_ignored_tac"]["origin_rel"][theory][pos][neg][prec] = f1_no_ign
-        # stat["acc"]["origin_rel"][theory][pos][neg] = acc
+        # stat["f1_no_ignored_tac"]["origin_rel"][theory][pos][neg][prec] = f1_no_ign
     elif ("origin" in splits) & ("prop" in splits):
         stat["f1"]["origin_prop"][theory][pos][neg][prec] = f1
-        stat["f1_no_ignored_tac"]["origin_prop"][theory][pos][neg][prec] = f1_no_ign
-        # stat["acc"]["origin_prop"][theory][pos][neg] = acc
+        # stat["f1_no_ignored_tac"]["origin_prop"][theory][pos][neg][prec] = f1_no_ign
     else:
         warnings.warn("skip " + os.path.join(root, ilp_stat_f))
 
@@ -111,31 +108,8 @@ def update_theory_stats(dir, stat, theory):
                 pos = int(info["pos"])
                 neg = int(info["neg"])
                 prec = int(info["prec"])
-                update_theory_stat(stat, f, root, theory, pos, neg, prec)
-
-
-def update_knn(dir, theory, stat):
-    file = os.path.join(dir, f"{theory}_stat.json")
-    reader = open(file, "r")
-    knn_stat = json.load(reader)
-    stat[theory] = knn_stat["accs"]
-
-
-def cal_aver_f1(stat, key):
-    aver_f1 = init_aver_f1()
-    for kind, kind_stat in stat[key].items():
-        for theory_stat in kind_stat.values():
-            for pos, pos_stat in theory_stat.items():
-                for neg, f1 in pos_stat.items():
-                    aver_f1[kind][pos][neg].append(f1)
-
-    for kind, kind_stat in aver_f1.items():
-        for pos, pos_stat in kind_stat.items():
-            for neg, f1s in pos_stat.items():
-                aver_f1[kind][pos][neg] = np.average(f1s)
-
-    for kind in stat[key].keys():
-        stat[key][kind]["aver"] = aver_f1[kind]
+                if prec in precs:
+                    update_theory_stat(stat, f, root, theory, pos, neg, prec)
 
 
 theories = ["theories/Lists"]
@@ -143,24 +117,15 @@ theories = ["theories/Lists"]
 # theories = ["theories/Init"]
 
 ilp_stat = {
-    # "acc": init_stat(theories),
     "f1": init_stat(theories),
-    "f1_no_ignored_tac": init_stat(theories),
 }
 # print(ilp_stat)
 for theory in theories:
     knn_stat = init_knn_stat(theory)
-    dir = f"data/json/origin_feat/tune/QArith/test_theory/{theory}"
+    dir = f"data/json/ortho/feat/tune/QArith/test_theory/{theory}"
     # dir = os.path.join(stat_i, theory)
     # update_knn(dir, theory, knn_stat)
     update_theory_stats(dir, ilp_stat, theory)
 
-
-# cal_aver_f1(ilp_stat, "f1")
-# cal_aver_f1(ilp_stat, "f1_no_ignored_tac")
-
-with open("QArith_tune_debug.json", "w") as w:
+with open("full_tune_ortho.json", "w") as w:
     json.dump(ilp_stat, w)
-
-# with open("knn_List.json", "w") as w:
-#     json.dump(knn_stat, w)
