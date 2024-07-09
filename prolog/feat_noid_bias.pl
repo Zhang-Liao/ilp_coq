64e4bb112acb4559ab5301d4336f4aced8fa720d
Zhang-Liao <zhangliao714@gmail..com>
2024-07-09 09:31:41 +0000

    rename

--- prolog/feat_noid_bias.pl
+++ prolog/feat_noid_bias.pl
@@ -1,15 +1,15 @@
-:- consult('/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym_rel_id_bk.pl').
+:- consult('/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/rel_noid_bk.pl').
 
 :- modeb(*, dif(+goal_idx, +goal_idx)).
 :- modeb(*, goal_position_left(+goal_idx, +goal_idx)).
 :- modeb(*, hyp_position_left(+hyp_idx, +hyp_idx)).
 :- modeb(*, goal_above(+nat, +goal_idx, +goal_idx)).
 :- modeb(*, hyp_above(+nat, +hyp_idx, +hyp_idx)).
+:- modeb(*, is_goal_root(+nat, +goal_idx)).
+:- modeb(*, is_hyp_root(+nat, +hyp_idx)).
 :- modeb(*, eq_goal_term(+nat, +goal_idx, +goal_idx)).
 :- modeb(*, eq_goal_hyp_term(+nat, +goal_idx, +hyp_idx)).
 :- modeb(*, eq_hyp_term(+nat, +hyp_idx, +hyp_idx)).
-:- modeb(*, is_goal_root(+nat, +goal_idx)).
-:- modeb(*, is_hyp_root(+nat, +hyp_idx)).
 :- modeb(*, similar_goal_terms(+nat, +goal_idx, +goal_idx)).
 
 :- determination(tac/2, dif/2).
@@ -20,8 +20,8 @@
 :- determination(tac/2, is_goal_root/2).
 :- determination(tac/2, is_hyp_root/2).
 :- determination(tac/2, eq_goal_term/3).
-:- determination(tac/2, eq_goal_hyp_term/3).
 :- determination(tac/2, eq_hyp_term/3).
+:- determination(tac/2, eq_goal_hyp_term/3).
 :- determination(tac/2, similar_goal_terms/3).
 
 % :- set(construct_bottom, false).
