78a6c38f6f1b56c16c7b5e99131f8423b7c4a4be
Zhang-Liao <zhangliao714@gmail..com>
2024-07-10 08:41:57 +0000

    feat

--- shell/train_anonym_theory_filter_theory.sh
+++ shell/train_anonym_theory_filter_theory.sh
@@ -1,18 +1,18 @@
-kind=rel
+kind=feat
 anonym=anonym
 param=p1n32
 train_theory=Structures
 descript=''
 knn_pred=data/json/feat/tune/$train_theory/test_theory
 test=data/json/predicate/anonym/merge/test$descript/
-clause=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/feat/tune/Structures/test_theory/valid/valid/rel/anonym/p1n32/good/18/alltac_rule.pl
+clause=/home/zhangliao/ilp_out_coq/ilp_out_coq/data/json/feat/tune/Structures/test_theory/valid/valid/feat/anonym/p1n32/good/18/alltac_rule.pl
 # theories=('plugins/rtauto' 'theories/FSets' 'theories/Wellfounded' 'plugins/funind' 'plugins/btauto' 'plugins/nsatz' 'theories/MSets')
 # theories=('plugins/rtauto' 'theories/Wellfounded' 'plugins/funind' 'plugins/btauto' 'plugins/nsatz')
 theories=('theories/MSets' 'theories/FSets')
 
 
 valid=valid
-bk=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym_rel_id_bk.pl
+bk=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/anonym_feat_id_bk.pl
 
 for theory in ${theories[@]}; do
     (
--- shell/train_feat_theory_filter_theory.sh
+++ shell/train_feat_theory_filter_theory.sh
@@ -3,13 +3,13 @@ anonym=origin
 param=p1n1
 knn_pred=data/json/feat/tune/$train_theory/test_theory
 test=data/json/predicate/$anonym/merge/test/
-clause=data/json/feat/tune/Structures/test_theory/valid/valid/rel/origin/p1n1/good/18/alltac_rule.pl
+clause=data/json/feat/tune/Structures/test_theory/valid/valid/feat/origin/p1n1/good/18/alltac_rule.pl
 # theories=('plugins/rtauto' 'theories/FSets' 'theories/Wellfounded' 'plugins/funind' 'plugins/btauto' 'plugins/nsatz' 'theories/MSets')
 # theories=('plugins/rtauto' 'theories/Wellfounded' 'plugins/funind' 'plugins/btauto' 'plugins/nsatz')
 theories=('theories/MSets' 'theories/FSets')
 
 valid=valid
-bk=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/rel_noid_bk.pl
+bk=/home/zhangliao/ilp_out_coq/ilp_out_coq/prolog/feat_noid_bk.pl
 
 for theory in ${theories[@]}; do
     (python filter.py --clause $clause \
@@ -17,5 +17,5 @@ for theory in ${theories[@]}; do
         --test $test/$theory \
         --label data/json/feat/merge/$theory.label \
         --bk $bk \
-        --info $valid/rel/$anonym/$param) &
+        --info $valid/feat/$anonym/$param) &
 done
